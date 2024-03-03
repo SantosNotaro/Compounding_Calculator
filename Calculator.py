96f19db1ea33165f43d4db3648d63800cd2834d6
SantosNotaro <SantosAnthonyNotaro@gmail.com>
2024-03-03 11:30:52 -0500

    Auto Restart Code/Time Delay

--- Calculator.py
+++ Calculator.py
@@ -9,15 +9,15 @@ def AutoRestartCode():
         return FV
 
     # Get input from the user
-    M = float(input("How much money is in your account?: "))
-    APR = float(input("What is your Annual Percentage Rate?: "))
+    M = float(input("How much money is in your account?: ").replace('$', '').replace(',', ''))
+    APR = float(input("What is your Annual Percentage Rate?: ").replace('%', ''))
     Y = int(input("How many years has this been compounding?: "))
 
     # Call the compound_interest function to calculate the future value
     FV = compound_interest(M, APR, Y)  # FV
 
     # Print the result
-    print(f"The future value after {Y} years is: {FV:.2f}")
+    print(f"The future value after {Y} years is: ${FV:.2f}")
 
 def main():
     while True:
